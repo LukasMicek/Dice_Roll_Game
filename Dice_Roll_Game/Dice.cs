1f995b87c28891d1e4a473d77e9aafbae5f479ad
Łukasz Mickiewicz <lukaszmickiewicz32@gmail.com>
2023-12-16 21:28:14 +0100

    added dice class

---
+++ Dice_Roll_Game/Dice.cs
@@ -0,0 +1,11 @@
+﻿
+public class Dice
+{
+    public int Number { get; } 
+    public int DiceRoll()
+    {
+        int _number = new Random().Next(1,7);
+        _number = Number;
+        return Number;
+    }
+}
--- Dice_Roll_Game/Program.cs
+++ Dice_Roll_Game/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿var dice = new Dice();
+var result = dice.DiceRoll();
+var triesLeft = new Tries();
+Console.WriteLine("Dice rolled. Guess what number it shows in 3 tries");
+Console.ReadKey();
+
+public class Tries
+{
+    public int tries = 3;
+    public int TryLost()
+    {
+        return tries--;
+    }
+}
